4049c8a665dfd4db2344622752800711dfabd1de
Chris Bieganek <cwbieganek@gmail.com>
2019-06-07 06:58:15 -0500

    Update description

---
+++ ndviarc.py
@@ -0,0 +1,10 @@
+# Description:
+# 	ArcPy geoprocessing script for computing NDVI (Normalized Difference Vegetation Index)
+#	for an input study area using NAIP imagery.
+
+# Inputs:
+# 	NAIP Imagery
+#	Study Area
+
+# Outputs:
+# 	GeoTIFF
